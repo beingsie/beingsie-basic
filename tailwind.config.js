ccf219a42643b61c63b1a1600e855c00cbf380c6
Beingsie <ada@beingsie.com>
2023-10-14 22:25:16 -0700

    feat: add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'twitter-blue': '#1D9BF0',
+      }
+    },
   },
   plugins: [],
 }
